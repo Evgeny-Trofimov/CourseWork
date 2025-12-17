f2799684a37cd749867db29c8bfd18283b870f5c
Evgeny-Trofimov <evgentrofimovevgen@yandex.ru>
2025-12-17 07:11:09 +0000

    Create RegisterDialog.h

---
+++ src/RegisterDialog.h
@@ -0,0 +1,27 @@
+#ifndef REGISTERDIALOG_H
+#define REGISTERDIALOG_H
+
+#include <QDialog>
+#include "Database.h"
+
+class QLineEdit;
+
+class RegisterDialog : public QDialog {
+    Q_OBJECT
+
+public:
+    RegisterDialog(Database* db, QWidget* parent = nullptr);
+    QString getRegisteredLogin() const { return m_registeredLogin; }
+
+private slots:
+    void onRegister();
+
+private:
+    Database* m_db;
+    QLineEdit* m_loginEdit;
+    QLineEdit* m_passEdit;
+    QLineEdit* m_passConfirmEdit;
+    QString m_registeredLogin;
+};
+
+#endif // REGISTERDIALOG_H
