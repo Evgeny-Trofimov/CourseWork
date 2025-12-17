4048cc3026bab8e588c2e43d762b38de572d880f
Evgeny-Trofimov <evgentrofimovevgen@yandex.ru>
2025-12-17 07:13:43 +0000

    Create ThreatForm.h

---
+++ src/ThreatForm.h
@@ -0,0 +1,28 @@
+#ifndef THREATFORM_H
+#define THREATFORM_H
+
+#include <QDialog>
+#include "Database.h"
+
+class QComboBox;
+class QLineEdit;
+class QTextEdit;
+
+class ThreatForm : public QDialog {
+    Q_OBJECT
+
+public:
+    ThreatForm(Database* db, QWidget* parent = nullptr);
+
+private slots:
+    void onSave();
+
+private:
+    Database* m_db;
+    QLineEdit* m_nameEdit;
+    QComboBox* m_typeCombo;
+    QComboBox* m_riskCombo;
+    QTextEdit* m_descEdit;
+};
+
+#endif 
